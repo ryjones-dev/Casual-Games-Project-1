ea1a45bd7144b953d59bd5bca6bfa3b52d756c82
Ryan Gaido (RIT Student) <rwg6766@ad.rit.edu>
2017-02-13 14:51:51 -0500

    Rough mockup of options menu created

---
+++ Project 1/Assets/Scripts/OptionsScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsScript : MonoBehaviour {
+
+    Canvas optionsMenu;
+
+	// Use this for initialization
+	void Start () {
+
+        optionsMenu = GameObject.Find("OptionsMenu").GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("escape"))
+        {
+            optionsMenu.enabled = !optionsMenu.enabled;
+        }
+    }
+}
