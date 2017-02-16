27040ca52e719cfe7533cdfa867aaea14a2f87df
Logan <loganr365@yahoo.com>
2017-02-16 14:03:02 -0500

    PickUp Script update.  Now drop item when Spacebar pressed, if an object is currently being held.

--- Project 1/Assets/Scripts/PickUp.cs
+++ Project 1/Assets/Scripts/PickUp.cs
@@ -10,11 +10,18 @@ public class PickUp : MonoBehaviour {
     // Use this for initialization
     void Start () {
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (heldObject != null)
+            {
+                dropObject(heldObject);
+            }
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
