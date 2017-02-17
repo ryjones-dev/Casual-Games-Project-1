cc53de1d4143c28fb95cf8c2d39301877d25f54a
Logan <loganr365@yahoo.com>
2017-02-17 13:34:01 -0500

    Added Cooldown to pick up, bassed on updates, not frames.  Will revisit in the future.  This prevents from automaticlly picking up an object as soon as you drop it.

--- Project 1/Assets/Scripts/PickUp.cs
+++ Project 1/Assets/Scripts/PickUp.cs
@@ -6,6 +6,9 @@ public class PickUp : MonoBehaviour {
     public GameObject hand;
     private bool objectInHand = false;
     private Collision heldObject;
+    private const int PICK_UP_COOLDOWN = 20;
+    private int currentCooldDown = 0;
+    private bool onCooldown = false;
 
     // Use this for initialization
     void Start () {
@@ -14,29 +17,47 @@ public class PickUp : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            if (heldObject != null)
+        if (!onCooldown)
+        { 
+            if (Input.GetKeyDown("space"))
             {
-                dropObject(heldObject);
+                if (heldObject != null)
+                {
+                    dropObject(heldObject);
+                    onCooldown = true;
+                }
+            }
+        }
+        else
+        {
+            currentCooldDown++;
+            if(currentCooldDown > PICK_UP_COOLDOWN)
+            {
+                currentCooldDown = 0;
+                onCooldown = false;
             }
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (objectInHand) {
-            return;
-        }
-        
-        if (collision.gameObject.tag == "intractable"){
-            collision.gameObject.transform.parent = hand.transform;
-            objectInHand = true;
-            heldObject = collision;
-            collision.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-            //collision.gameObject.GetComponent<Rigidbody>().useGravity = false;
-            //collision.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-            //rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        if (!onCooldown)
+        {
+            if (objectInHand)
+            {
+                return;
+            }
+
+            if (collision.gameObject.tag == "intractable")
+            {
+                collision.gameObject.transform.parent = hand.transform;
+                objectInHand = true;
+                heldObject = collision;
+                collision.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                //collision.gameObject.GetComponent<Rigidbody>().useGravity = false;
+                //collision.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+                //rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+            }
         }
     }
 
