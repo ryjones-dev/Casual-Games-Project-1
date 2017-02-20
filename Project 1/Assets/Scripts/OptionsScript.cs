45635d130918162d0d3ae3e5ee11588086355f67
Ryan Gaido (RIT Student) <rwg6766@ad.rit.edu>
2017-02-20 14:56:43 -0500

    fixed resolution issues added ESC panel

--- Project 1/Assets/Scripts/OptionsScript.cs
+++ Project 1/Assets/Scripts/OptionsScript.cs
@@ -2,32 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class OptionsScript : MonoBehaviour {
 
-    Canvas optionsMenu;
+    //Canvas optionsMenu;
 
+    public GameObject settingsPanel;
+    public GameObject escPanel;
     public Slider mouse;
     public float mouseSensitivity=1.0f;
     public Slider music;
     public float musicVolume = 1.0f;
     public Slider sound;
     public float soundEffectVolume = 1.0f;
-    public Toggle invertMouse;
-    public bool mouseInverted = false;
+    public Toggle invertMouseMovement;
+    public bool mouseMovementInverted = false;
+    public Toggle invertMouseRotation;
+    public bool mouseRotationInverted = false;
 
-	// Use this for initialization
-	void Start () {
-        
-        optionsMenu = GetComponent<Canvas>();
-        optionsMenu.enabled = false;
+    private CursorLockMode prevLockMode;
+
+    // Use this for initialization
+    void Start () {
+
+        //optionsMenu = GetComponent<Canvas>();
+        //optionsMenu.enabled = false;
+        settingsPanel.SetActive(false);
+        escPanel.SetActive(true);
 
         //audio is not yet implemented
-        music.enabled = false;
-        sound.enabled = false;
+        //music.enabled = false;
+        //sound.enabled = false;
 
         mouse.value = mouseSensitivity;
-        invertMouse.isOn = mouseInverted;
+        invertMouseMovement.isOn = mouseMovementInverted;
+        invertMouseRotation.isOn = mouseRotationInverted;
         music.value = musicVolume;
         sound.value = soundEffectVolume;
 
@@ -38,33 +48,61 @@ public class OptionsScript : MonoBehaviour {
 	void Update () {
         if (Input.GetKeyDown("escape"))
         {
-            if (optionsMenu.enabled)
+            //escPanel.SetActive(!escPanel.activeSelf);
+            //if (optionsMenu.enabled)
+            if(settingsPanel.activeSelf)
             {
                 CancelSettings();
             }
             else
             {
-                optionsMenu.enabled = true;
+                Cursor.visible = true;
+                prevLockMode = Cursor.lockState;
+                Cursor.lockState = CursorLockMode.None;
+
+                settingsPanel.SetActive(true);
+                escPanel.SetActive(false);
+                //optionsMenu.enabled = true;
             }
+
+           // optionsMenu.enabled = true;
             //optionsMenu.enabled = !optionsMenu.enabled;
         }
     }
 
     public void ChangeSettings()
     {
+        Cursor.visible = false;
+        Cursor.lockState = prevLockMode;
+
         mouseSensitivity = mouse.value;
-        mouseInverted = invertMouse.isOn;
+        mouseMovementInverted = invertMouseMovement.isOn;
+        mouseRotationInverted = invertMouseRotation.isOn;
         musicVolume = music.value;
         soundEffectVolume = sound.value;
-        optionsMenu.enabled = false;
+        //optionsMenu.enabled = false;
+        settingsPanel.SetActive(false);
+        escPanel.SetActive(true);
+
+        //deselect button, otherwise last-hit button will appear to be highlighted until a new button is hit, 
+        //seems like weird default behavior personally
+        EventSystem.current.SetSelectedGameObject(null);
     }
 
     public void CancelSettings()
     {
+        Cursor.visible = false;
+        Cursor.lockState = prevLockMode;
+
         mouse.value = mouseSensitivity;
-        invertMouse.isOn = mouseInverted;
+        invertMouseMovement.isOn = mouseMovementInverted;
+        invertMouseRotation.isOn = mouseRotationInverted;
         music.value = musicVolume;
         sound.value = soundEffectVolume;
-        optionsMenu.enabled = false;
+        //optionsMenu.enabled = false;
+        settingsPanel.SetActive(false);
+        escPanel.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
     }
 }
