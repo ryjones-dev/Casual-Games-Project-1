6ded8d8d04eb89ff5fea81f7dd109b1688075645
bluebug <iopzxz@gmail.com>
2017-02-27 11:51:56 -0500

    1
    1

---
+++ Project 1/Assets/Scripts/UI/AssetManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NUI
+{
+    //Sprite bank for UI
+    public class AssetManager : MonoBehaviour
+    {
+        public static AssetManager ME;
+        public Sprite SPRT_ERROR;
+        public Sprite
+            SPRT_LOTION,
+            SPRT_CLOCK,
+            SPRT_SENDWITCH,
+            SPRT_REGISTERED,
+            SPRT_UNREGISTERED,
+            SPRT_REGISTERED_ERROR;
+
+        void Awake() {
+            ME = this;
+        }
+    }
+
+}
---
+++ Project 1/Assets/Scripts/UI/ItemReference.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NUI {
+
+    //Reference describing what the item in the world is and what kinds of status it is.
+    public enum ItemType {LOTION,CLOCK,SENDWITCH }
+    public enum ItemStatus { UNREGISTERED, REGISTERED, REGISTERE_ERROR }
+    public class ItemReference : MonoBehaviour
+    {
+        public ItemType m_type;
+        public ItemStatus m_status;
+
+    }
+
+}
