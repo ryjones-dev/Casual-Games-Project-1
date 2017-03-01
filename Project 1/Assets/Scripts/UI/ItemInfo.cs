7240f18d92a4c2ed6da3422e0d567892ef295780
BlueBug <iopzxz@gmail.com>
2017-03-01 13:29:47 -0500

    I changed namespace

---
+++ Project 1/Assets/Scripts/UI/ItemInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+namespace NGame
+{
+    
+    public class ItemInfo : MonoBehaviour
+    {
+        [SerializeField]
+        ITEM_TYPE m_type;
+        public ITEM_STATUS m_status;
+        [SerializeField]
+        List<ITEM_PURPOSE> m_purposes;
+        
+        public ITEM_TYPE Type { get { return m_type; } }
+        //public ITEM_STATUS Status { get { return m_status; } set { m_status = value; } }
+        public List<ITEM_PURPOSE> Purposes { get {
+                return m_purposes.Select(item => (ITEM_PURPOSE)item).ToList();
+            }
+        }
+
+    }
+
+}
