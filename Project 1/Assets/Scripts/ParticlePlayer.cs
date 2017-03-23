0c66d5ab24e79b0bb1ca1592a1b887c0552a0656
MAIN\ltr6702 <loganr365@yahoo.com>
2017-03-23 16:14:21 -0400

    Particle Update

---
+++ Project 1/Assets/Scripts/ParticlePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePlayer : MonoBehaviour {
+    public GameObject particleSys;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void playWinParticle()
+    {
+        if (!particleSys.GetComponent<ParticleSystem>().isPlaying)
+        {
+            particleSys.GetComponent<ParticleSystem>().Play();
+        }
+    }
+}
